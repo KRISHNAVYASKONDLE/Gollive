d755b534db0690b8c03db0186c345d3f63434e0d
krishnavyas kondle <krishnavyaskondle@gmail.com>
2021-06-11 00:34:54 +0530

    added  match details

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Tabs, Tab, AppBar } from "@material-ui/core";
 import Nav from "./../Nav/Nav";
 import Nav2 from "./../Nav2/Nav2";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
   const [value, setValue] = React.useState(0);
   const handleTabs = (e, val) => {
     setValue(val);
   };
+  const [selectedDate, handleDateChange] = useState(new Date());
   return (
     <div className="app">
       <Nav />
@@ -27,10 +34,34 @@ function App() {
               </Tabs>
             </AppBar>
           </div>
-
           <Tabpanel value={value} index={0}>
+            <div className="match-details">
+              <div className="matchdetails-header">match details</div>
+              <div className="selected-tournament">selected tournament</div>
+              <div className="Skyleague-tournament">Sky league tournament</div>
+              <div className="input-container">
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    autoOk
+                    variant="inline"
+                    inputVariant="outlined"
+                    format="MM/dd/yyyy"
+                    value={selectedDate}
+                    InputAdornmentProps={{ position: "end" }}
+                    onChange={(date) => handleDateChange(date)}
+                  />
+                </MuiPickersUtilsProvider>
+                <TextField
+                  id="outlined-basic"
+                  label="MATCH TYPE"
+                  variant="outlined"
+                />
+              </div>
+            </div>
             Basics
           </Tabpanel>
+
+          {/* <DatePicker value={Date} onChange={setDate} /> */}
           <Tabpanel value={value} index={1}>
             MAP PICKS
           </Tabpanel>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 import React from "react"
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-ReactDOM.render(<App />,document.getElementById("root"));
+//import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+//import DateFnsUtils from '@date-io/date-fns';
+//import 'date-fns'
+
+
+ReactDOM.render(
+//<MuiPickersUtilsProvider utils={DateFnsUtils}><App /></MuiPickersUtilsProvider>,document.getElementById("root"));
+<App />,document.getElementById("root"));
