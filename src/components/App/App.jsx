0fe918367e48522d8dc1cdd43f31363699fc93bb
krishnavyas kondle <krishnavyaskondle@gmail.com>
2021-06-24 23:34:40 +0530

    added toaster and helper text

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,8 +16,15 @@ import TeamBadge from "./../../assets/TeamBadge.png";
 import Thirdpage from "../Thirdpage/Thirdpage";
 import Button from '@material-ui/core/Button';
 import Axios from "axios";
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+import hi from "date-fns/esm/locale/hi/index.js";
 
 // styles for select components
+function Alert(props) {
+  return <MuiAlert elevation={15} variant="filled" {...props} />;
+}
+
 const selectStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
@@ -28,7 +35,49 @@ const selectStyles = makeStyles((theme) => ({
   },
 }));
 
+
 function App() {
+
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+
+    setOpen(true);
+    setCurrentTab(1);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  let [dispHelptext, setdispHelptext] = React.useState("");
+
+
+  const [hometeamscore, sethometeamscore] = React.useState(0);
+  let handlehometeamscore = (event) => {
+    sethometeamscore(event.target.value);
+    (hometeamscore < 10) ? setdispHelptext("") : setdispHelptext("Enter Values < 100");
+
+  }
+  let [guestdispHelptext, setguestdispHelptext] = React.useState("");
+
+  const [guestteamscore, setguestteamscore] = React.useState(0);
+
+
+  let handleguestteamscore = (event) => {
+    setguestteamscore(event.target.value);
+    (guestdispHelptext < 10) ? setguestdispHelptext("") : setguestdispHelptext("Enter Values < 100");
+
+  }
+
+
+
+
   const classes = selectStyles();
 
   // tabs selection
@@ -92,6 +141,12 @@ function App() {
     <div className="app">
       <Nav />
       <div className="main-container">
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="success">
+            Data submitted successfully!
+          </Alert>
+        </Snackbar>
+
         <div className="left-nav-container">
           <Nav2 />
         </div>
@@ -158,8 +213,15 @@ function App() {
                   </FormControl>
                 </div>
                 <div className="teamScores-container add-flex-class">
-                  <TextField id="homeTeamScore" label="TYPE HOME TEAM SCORE" variant="outlined" />
-                  <TextField id="guestTeamScore" label="TYPE GUEST TEAM SCORE" variant="outlined" />
+                  <TextField id="homeTeamScore" type="number" InputProps={{
+                    inputProps: { max: 25, min: 1 }
+                  }} onChange={handlehometeamscore}
+                    helperText={dispHelptext}
+                    label="TYPE HOME TEAM SCORE" variant="outlined" />
+                  <TextField id="guestTeamScore" type="number" InputProps={{
+                    inputProps: { max: 25, min: 1 }
+                  }} onChange={handleguestteamscore}
+                    helperText={guestdispHelptext} label="TYPE GUEST TEAM SCORE" variant="outlined" />
                 </div>
               </div>
 
@@ -172,17 +234,22 @@ function App() {
                   </div>
                   <div className="add-flex-class">
                     <TextField spacing="1" id="result3.0" label="TYPE POLL PERCENTAGE" variant="outlined" />
-                    <TextField spacing="1" id="result3.0" label="TYPE POLL PERCENTAGE" variant="outlined" />
+                    <TextField spacing="1" id="result3.2" label="TYPE POLL PERCENTAGE" variant="outlined" />
+
                   </div>
+
                 </form>
               </div>
               <div className="nextdirector">
                 <div className="nextdet-container">
-                  <Button onClick={() => { setCurrentTab(1) }} variant="contained" color="primary">   NEXT   </Button>
+                  <Button onClick={handleClick} variant="contained" color="primary">   NEXT   </Button>
+
                 </div>
               </div>
               <div className="forapitest-container">
                 <button onClick={callApi} className="reqapi" variant="outlined" color="primary"  >hello</button>
+
+
               </div>
             </div>
 
@@ -358,71 +425,71 @@ function App() {
       </div>
     </div>
   );
-}
 
-function callApi() {
-  var data = JSON.stringify({
-    "@class": ".LogEventRequest",
-    "eventKey": "LeaugeTeamLBUpdate",
-    "leagueId": "Sky Esports League-1617165350190",
-    "leagueAdditionId": "Sky Esports League-1617165350190-March-2021-1617165492876",
-    "homeTeamScore": 3,
-    "awayTeamScore": 0,
-    "matchStartTime": 1619877600000,
-    "homeTeam": "Kolkata Tridents",
-    "awayTeam": "Punjab Pinnacles",
-    "matchId": "Sky Esports League-1617165350190-March-2021-1617165492876-Kolkata Tridents-1619877600000",
-    "matchLink": "",
-    "selectedByAwayTeam": [
-      {
-        "homeTeamScore": 13,
-        "awayTeamScore": 5,
-        "screenShot": "",
-        "videoLink": "",
-        "mapName": "ICEBOX"
-      }
-    ],
-    "selectedByHomeTeam": [
-      {
-        "homeTeamScore": 13,
-        "awayTeamScore": 10,
-        "screenShot": "",
-        "videoLink": "",
-        "mapName": "HAVEN"
+
+  function callApi() {
+    var data = {
+      "@class": ".LogEventRequest",
+      "eventKey": "LeaugeTeamLBUpdate",
+      "leagueId": "Sky Esports League-1617015905529",
+      "leagueAdditionId": "Sky Esports League-1617015905529-2021-March-1617025320377",
+      "homeTeamScore": hometeamscore,
+      "awayTeamScore": 2,
+      "matchStartTime": 1619244230000,
+      "homeTeam": "Chennai Clutchers",
+      "awayTeam": "Bengaluru Crushers",
+      "matchId": "Sky Esports League-1617015905529-2021-March-1617025320377-Chennai Clutchers-1619244230000",
+      "matchLink": "",
+      "selectedByAwayTeam": [
+        {
+          "homeTeamScore": 13,
+          "awayTeamScore": 5,
+          "screenShot": "",
+          "videoLink": "",
+          "mapName": "ICEBOX"
+        }
+      ],
+      "selectedByHomeTeam": [
+        {
+          "homeTeamScore": 13,
+          "awayTeamScore": 10,
+          "screenShot": "",
+          "videoLink": "",
+          "mapName": "HAVEN"
+        },
+        {
+          "homeTeamScore": 13,
+          "awayTeamScore": 8,
+          "screenShot": "",
+          "videoLink": "",
+          "mapName": "ASCENT"
+        }
+      ],
+      "homeTeamTieBreaker": 16,
+      "awayTeamTieBreaker": -16,
+      "playerId": "60378db593d80404f7a18234"
+    };
+
+    console.log(data);
+    var config = {
+      method: 'post',
+      url: 'https://h391398vkuek.preview.gamesparks.net/rs/debug/UZVkxRGqK5ewFbmMrEg2yuseepqcm5Tf/LogEventRequest',
+      headers: {
+        'Content-Type': 'application/json'
       },
-      {
-        "homeTeamScore": 13,
-        "awayTeamScore": 8,
-        "screenShot": "",
-        "videoLink": "",
-        "mapName": "ASCENT"
-      }
-    ],
-    "homeTeamTieBreaker": 16,
-    "awayTeamTieBreaker": -16,
-    "playerId": "60378db593d80404f7a18234"
-  });
+      data: data
+    };
 
-  var config = {
-    method: 'post',
-    url: 'https://h391398vkuek.preview.gamesparks.net/rs/debug/UZVkxRGqK5ewFbmMrEg2yuseepqcm5Tf/LogEventRequest',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data: data
-  };
+    Axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
 
-  Axios(config)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-
+  }
 }
-
 function Tabpanel(props) {
   const { children, value, index } = props;
   return <div>{value === index && <h1>{children}</h1>}</div>;
